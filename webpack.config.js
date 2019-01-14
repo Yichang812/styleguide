4d628d7c7756048c226e3f67e41c7a64151cdb87
Yichang <yichang.li@zalora.com>
2019-01-14 15:47:45 +0800

    test

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,7 @@ module.exports = {
         }),
         new HtmlWebpackPlugin({
             filename: '../index.html',
-            template: '_template/index.html'
+            template: 'template/index.html'
         }),
     ],
 
